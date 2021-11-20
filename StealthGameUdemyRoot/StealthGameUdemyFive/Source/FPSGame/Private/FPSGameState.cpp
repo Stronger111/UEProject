d960415667be170e083111882b25f58d820c63c6
du776239125 <776239125@qq.com>
2021-11-20 18:39:49 +0800

    完結多人游戲
    完結多人游戲

---
+++ StealthGameUdemyRoot/StealthGameUdemyFive/Source/FPSGame/Private/FPSGameState.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSGameState.h"
+#include "FPSPlayerController.h"
+#include "Engine/World.h"
+
+void AFPSGameState::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bMissionSuccess)
+{
+	for (FConstPlayerControllerIterator It=GetWorld()->GetPlayerControllerIterator();It;++It)
+	{
+		AFPSPlayerController* PC = Cast<AFPSPlayerController>(It->Get());
+		if (PC && PC->IsLocalController())
+		{
+			PC->OnMissionCompleted(InstigatorPawn, bMissionSuccess);
+
+			APawn* Pawn = PC->GetPawn();
+			if (Pawn)
+			{
+				Pawn->DisableInput(nullptr);
+			}
+		}
+
+
+	}
+	
+}
